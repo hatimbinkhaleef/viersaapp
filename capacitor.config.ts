959db00b50277b532ad527be086a4ede805fe492
hatimbinkhaleef <117269672+hatimbinkhaleef@users.noreply.github.com>
2022-11-03 16:07:46 +0530

    ok

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.jamesonsaunders.convertwebsitetoapp',
-  appName: 'Website to App',
+  appId: 'com.viersa.app',
+  appName: 'viersa',
   webDir: 'www',
   bundledWebRuntime: false,
 };
